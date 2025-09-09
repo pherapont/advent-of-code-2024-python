526baabe1e23224912af995686c8f36770cfe9c3
pherapont <pherapont@yandex.ru>
2025-09-10 00:00:27 +0700

    complete day2

--- day2/safe_reports.py
+++ day2/safe_reports.py
@@ -6,15 +6,42 @@
     б) Разность соседних чисел на больше 3
 
 Оба эти действия выделяются в отдельные функции и хорошо тестируютс
+How many reports are safe
+Part2:
+    We can remove one bad level and report become safe
 '''
+
 
 def get_nums(line: str) -> list[int]:
     return [int(num) for num in line.split()]
 
+
 def check_report(report: list[int]) -> bool:
     diff = [x[1] - x[0] for x in zip(report[:], report[1:])]
-    print (diff)
-    return True
+    return all(3 >= x > 0 for x in diff) or all(0 > x >= -3 for x in diff)
+
+
+def check_light_report(report: list[int]) -> bool:
+    for i, number in enumerate(report):
+        light_report = report[:i] + report[i+1:]
+        if check_report(light_report):
+            return True
+    return False
+
+
+def main(filename: str):
+    res = 0
+    with open(filename, 'r') as f:
+        for line in f:
+            numbers = get_nums(line)
+            if check_report(numbers):
+                res += 1
+            else:
+                if check_light_report(numbers):
+                    res += 1
+    return res
+
 
 if __name__ == '__main__':
-    check_report([1, 4, 5, 2])
+    res = main('data.txt')
+    print(res)
--- day2/safe_reports_test.py
+++ day2/safe_reports_test.py
@@ -1,8 +1,9 @@
 import unittest
-from safe_reports import get_nums, check_report
+from safe_reports import get_nums, check_report, check_light_report
+
 
 class TestSafeReports(unittest.TestCase):
-    
+
     def test_get_only_nums(self):
         data = "125 222 333 44"
         output = [125, 222, 333, 44]
@@ -17,6 +18,29 @@ class TestSafeReports(unittest.TestCase):
         data = [125, 126, 128, 131]
         self.assertTrue(check_report(data))
 
+    def test_check_safe_decrease_report(self):
+        data = [432, 430, 427, 426]
+        self.assertTrue(check_report(data))
+
+    def test_check_to_large_step_report(self):
+        data = [1, 10, 20]
+        self.assertFalse(check_report(data))
+
+    def test_check_null_step_report(self):
+        data = [10, 10]
+        self.assertFalse(check_report(data))
+
+    def test_check_safe_light_report(self):
+        data = [10, 9, 11, 14]
+        self.assertTrue(check_light_report(data))
+
+    def test_check_safe_light_report_with_equal(self):
+        data = [22, 22, 24, 29]
+        self.assertFalse(check_light_report(data))
+
+    def test_check_light_report_unsafe(self):
+        data = [33, 37, 42, 43]
+        self.assertFalse(check_light_report(data))
 
 if __name__ == '__main__':
     unittest.main()
