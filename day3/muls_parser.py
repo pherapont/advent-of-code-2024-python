9459703bb9e83f292471bf3d838e3690b00dde8c
pherapont <pherapont@yandex.ru>
2025-09-10 20:44:37 +0300

    day3 parser for mul

--- day3/muls_parser.py
+++ day3/muls_parser.py
@@ -6,11 +6,26 @@
 import re
 
 
-def parser(text: str) -> str:
+def parser(text: str) -> list[str]:
     res = re.findall(r'mul\(\d+,\d+\)', text)
     return res
 
+def extract_nums_from(mul: str) -> list(int):
+    '''
+    Функция извлекает два числа из строки
+    типа "mul(213,432)" с числами разной разрядности
+    '''
+    ...
+
+
+def main(datafile: str) -> int:
+    data = []
+    with open(datafile, 'r') as df:
+        for line in df:
+            data.append(parser(line))
+
+    print(data)
+    return 0
 
 if __name__ == '__main__':
-    res = parser('formwhy()?mul(603,692)({sel}')
-    print(res)
+    main('data.txt')
