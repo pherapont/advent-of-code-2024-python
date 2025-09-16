e52a29e3e25e6159eed13cf78a5f4f2c308e6277
pherapont <pherapont@yandex.ru>
2025-09-16 15:57:50 +0300

    finish day4 part 1

--- day4/xmas_search.py
+++ day4/xmas_search.py
@@ -1,75 +1,56 @@
-"""
-Метод поиска 'XMAS':
-    - чтобы не дублироваться начинаем искать только с 'X'
-    - ищем в 8 направлениях
-    - прерываем поиск при отклонении от шаблона и при выходе за границу матрицы
-    - при нахождении совпадения - просто увеличиваем счетчик
-"""
-goal = 'XMAS'
-
-def horisont_forward(y: int, x: int, xmax: int) -> list[tuple[int]]:
-    res = []
-    for i in range(1,4):
-        xi = x + i
-        if xi > xmax:
-            return None
-        res.append((y, xi))
-    return res
-
-def horisont_back(y: int, x: int) -> list[tuple[int]]:
-    res = []
-    for i in range(1,4):
-        xi = x - i
-        if xi < 0:
-            return None
-        res.append((y, xi))
-    return res
-
 def search_tails(data: list[str],
-        y: int, x: int, ymax: int, xmax: int) -> int:
-    res = 0
-    if hf := horisont_forward(y, x, xmax):
-        print(hf)
-        for i in range(3):
-            ind_y = hf[i][0]
-            ind_x = hf[i][1]
-            print("put", data[ind_y][ind_x])
-            print("goal", goal[i+1])
-            if data[ind_y][ind_x] == goal[i+1]:
-                if i == 2:
-                    res += 1
-                    print('bingo, res = ', res)
-            else: break
-    if hb := horisont_back(y, x):
-        for i in range(3):
-            ind_y = hb[i][0]
-            ind_x = hb[i][1]
-            print("put", data[ind_y][ind_x])
-            print("goal", goal[i+1])
-            if data[ind_y][ind_x] == goal[i+1]:
-                if i == 2:
-                    res += 1
-                    print('bingo, res = ', res)
-            else: break
-    return res
-        
-
-def search_xmas(data: list[str]) -> int:
+                 y0: int, x0: int) -> int:
     ymax = len(data) - 1
     xmax = len(data[0]) - 1
+    vectors = {'hf': [(0,1),(0,2),(0,3)],
+               'hb': [(0,-1),(0, -2),(0, -3)],
+               'vt': [(1,0), (2,0), (3,0)],
+               'vd': [(-1,0), (-2,0), (-3,0)],
+               'ft': [(1,1), (2,2), (3,3)],
+               'fd': [(-1,1), (-2,2), (-3,3)],
+               'bt': [(1,-1), (2,-2), (3,-3)],
+               'bd': [(-1,-1), (-2,-2), (-3,-3)]
+               }
+    res = 0
+
+    for vv in vectors.values():
+        word = 'X'
+        print("Begin search")
+        for shift in vv:
+            point = list(zip((y0,x0), shift))
+            y = sum(point[0])
+            x = sum(point[1])
+            print(f"y = {y}, x = {x}")
+            if y > ymax or y < 0 or x > xmax or x < 0:
+                print("BREAK")
+                print(f"xmax = {xmax}, ymax = {ymax}")
+                break
+            letter = data[y][x]
+            word += letter
+            print(word)
+        if word == 'XMAS':
+            res += 1
+    return res
+
+
+def search_xmas(data: list[str]) -> int:
     res = 0
     for ny, line in enumerate(data):
         for nx, letter in enumerate(line):
             if letter == 'X':
-                print('Search')
-                res += search_tails(data, ny, nx, ymax, xmax)
+                res += search_tails(data, ny, nx)
     return res
 
 
 def main(data_file: str) -> int:
     matrix = []
     with open(data_file) as df:
-        for line in df:
-            matrix.append(line)
+     for line in df:
+         matrix.append(line.strip())
     res = search_xmas(matrix)
     return res
+
+
+if __name__ == '__main__':
+    res = main("input_data.txt")
+    print(res)
--- day4/xmas_search_test.py
+++ day4/xmas_search_test.py
@@ -3,7 +3,6 @@ from xmas_search import search_xmas
 from test_data import origin_data, horiz_overlap
 
 class XmasSearchTest(unittest.TestCase):
-    @unittest.skip("Finish testing")
     def test_origin_data(self):
         data = origin_data.split()
         self.assertEqual(search_xmas(data), 18)
