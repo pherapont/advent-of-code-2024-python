39190fb60ea86428f61e77abe5be66212a937797
pherapont <pherapont@yandex.ru>
2025-09-18 17:50:51 +0700

    finish day5 part1

--- day5/correct_updates.py
+++ day5/correct_updates.py
@@ -19,18 +19,59 @@ def parser_data(data_file: str) -> tuple[list[str], list[str]]:
     return rules, updates
 
 
-def finalize_rules(row_rules: list[str]) -> dict[str, set[str]]:
+def filter_bad_updates(data: list[list[int]],
+                       rules: dict[int, set[int]]) -> list[list[int]]:
+    """
+    Проверяем числа стоящие после в каждом update, если они
+    встречаются в правиле, то это не корректный update
+    """
+    correct_update = []
+    for update in data:
+        flag = False
+        for pos, num in enumerate(update[:-1]):
+            if num in rules:
+                for check in update[pos+1:]:
+                    if check in rules[num]:
+                        flag = True
+                        break
+            if flag: break
+        if flag: continue    
+        correct_update.append(update)
+    return correct_update
+
+
+def transform_updates(row_updates: list[str]) -> list[list[int]]:
+    data = [x.split(',') for x in row_updates]
+    res = []
+    for chunk in data:
+        res.append([int(x) for x in chunk])
+    return res
+
+
+def finalize_rules(row_rules: list[str]) -> dict[int, set[int]]:
+    """
+    В правило (множество) каждого числа, включаем числа, которые
+    должны передшествовать этому числу
+    """
     rules = {}
     for line in row_rules:
         rule = line.split("|")
-        if rule[1] not in rules:
-            rules[rule[1]] = {rule[0]}
+        rule_key = int(rule[1])
+        rule_value = int(rule[0])
+        if rule_key not in rules:
+            rules[rule_key] = {rule_value}
         else:
-            rules[rule[1]].add(rule[0])
+            rules[rule_key].add(rule_value)
     return rules
 
 
 def main(data_file: str) -> int:
-    row_rules, updates = parser_data(data_file)
+    row_rules, row_updates = parser_data(data_file)
+    updates = transform_updates(row_updates)
     rules = finalize_rules(row_rules)
-    return 0
+    correct_updates = filter_bad_updates(updates, rules)
+    return sum([x[len(x) // 2] for x in correct_updates])
+
+if __name__ == '__main__':
+    res = main("input_data.txt")
+    print(res)
--- day5/correct_updates_test.py
+++ day5/correct_updates_test.py
@@ -1,10 +1,11 @@
 import unittest
-from correct_updates import parser_data, finalize_rules
+from correct_updates import (parser_data, finalize_rules,
+                             filter_bad_updates,
+                             transform_updates, main)
 
 class TestCorrectUpdates(unittest.TestCase):
-    @unittest.skip("for finish")
     def test_correct_update_data_from_site(self):
-        self.assetEqual()
+        self.assertEqual(main("test_data.txt"), 143)
 
     def test_parser_data(self):
         rules = ["1|1", "2|2", "133|585"]
@@ -12,10 +13,23 @@ class TestCorrectUpdates(unittest.TestCase):
         self.assertEqual(parser_data("test_parser_data.txt"),
                          (rules, updates))
 
+    def test_transform_updates(self):
+        row_updates = ["22,33,44", "75,43,21", "85,49,32,45"]
+        updates = [[22, 33, 44], [75, 43, 21], [85, 49, 32, 45]]
+        self.assertEqual(transform_updates(row_updates), updates)
+
     def test_finalize_rules(self):
         row_rules = ["1|1", "14|1", "21|2", "2|2", "133|585"]
-        rules = {"1": {"1", "14"}, "2": {"21", "2"}, "585": {"133"}}
+        rules = {1: {1, 14}, 2: {21, 2}, 585: {133}}
         self.assertEqual(finalize_rules(row_rules), rules)
+
+    def test_filter_bad_updates(self):
+        rules = {2: {1}, 8: {1, 2}, 14: {1, 2, 8}}
+        updates = [[1, 2, 8, 14], [2, 1, 8], [2, 14], [14, 1]]
+        correct_updates = [[1, 2, 8, 14], [2, 14]]
+        self.assertEqual(filter_bad_updates(updates, rules),
+                         correct_updates)
+
 
 if __name__ == '__main__':
     unittest.main()
