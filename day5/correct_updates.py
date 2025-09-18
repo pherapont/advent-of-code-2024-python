7292a6c26bee68418e27eadca9af13b8e0343b78
pherapont <pherapont@yandex.ru>
2025-09-18 14:57:58 +0300

    transform rules in dict

--- day5/correct_updates.py
+++ day5/correct_updates.py
@@ -2,27 +2,35 @@
 Из файла с данными необходимо выделить все корректные строки
 и найти сумму их средних членов.
 """
-def parser_data(data_file: str) -> tuple[list[str]]:
+def parser_data(data_file: str) -> tuple[list[str], list[str]]:
     with open(data_file) as df:
         updates_flag = False
         rules = []
         updates = []
         for line in df:
             line = line.strip()
-            print(line)
             if len(line) == 0:
-                print('Empty line')
                 updates_flag = True
                 continue
             if not updates_flag:
                 rules.append(line)
-                print(f"rules = {rules}")
             else:
                 updates.append(line)
-                print(f"updates = {updates}")
     return rules, updates
 
 
+def finalize_rules(row_rules: list[str]) -> dict[str, set[str]]:
+    rules = {}
+    for line in row_rules:
+        rule = line.split("|")
+        if rule[1] not in rules:
+            rules[rule[1]] = {rule[0]}
+        else:
+            rules[rule[1]].add(rule[0])
+    return rules
+
+
 def main(data_file: str) -> int:
-    rules, updates = parser_data(data_file)
-            
+    row_rules, updates = parser_data(data_file)
+    rules = finalize_rules(row_rules)
+    return 0
--- day5/correct_updates_test.py
+++ day5/correct_updates_test.py
@@ -1,5 +1,5 @@
 import unittest
-from correct_updates import parser_data
+from correct_updates import parser_data, finalize_rules
 
 class TestCorrectUpdates(unittest.TestCase):
     @unittest.skip("for finish")
@@ -11,6 +11,11 @@ class TestCorrectUpdates(unittest.TestCase):
         updates = ["22 33 44", "75 43 21", "85 49 32 45"]
         self.assertEqual(parser_data("test_parser_data.txt"),
                          (rules, updates))
+
+    def test_finalize_rules(self):
+        row_rules = ["1|1", "14|1", "21|2", "2|2", "133|585"]
+        rules = {"1": {"1", "14"}, "2": {"21", "2"}, "585": {"133"}}
+        self.assertEqual(finalize_rules(row_rules), rules)
 
 if __name__ == '__main__':
     unittest.main()
