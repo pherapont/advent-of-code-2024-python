c814f8189919cc6beadf6718faaad7c9cbef975e
pherapont <pherapont@yandex.ru>
2025-09-29 23:51:16 +0700

    realization of moving in room

--- day6/guard_in_room.py
+++ day6/guard_in_room.py
@@ -1,10 +1,12 @@
 """
-Сканирование движения охранника по комнате с подсчетом пройденных полей.
+Сканирование движения охранника по комнате
+с подсчетом пройденных полей.
 Rule of motion: move while achieve "#" than turn right
 """
 
 
-def data_preparation(file_name: str) -> tuple[list[list[int]], list[int]]:
+def data_preparation(file_name: str
+                     ) -> tuple[list[list[int]], list[int]]:
     with open(file_name) as f:
         struct_room = []
         init_pos: list[int]
@@ -23,16 +25,22 @@ def data_preparation(file_name: str) -> tuple[list[list[int]], list[int]]:
     return struct_room, init_pos
 
 
-def room_tour(room: list[list[int]], init_pos: list[int]) -> int:
+def room_tour(room: list[list[int]], init_pos: list[int]
+              ) -> int:
     right_bound = len(room[0])
     down_bound = len(room)
     pos = dict(zip(("y", "x"), (init_pos)))
     directions = ["up", "right", "down", "left"]
-    dir_rules = {"up": (-1, 0), "right": (0, 1), "down": (1, 0), "left": (0, -1)}
+    dir_rules = {"up": (-1, 0), "right": (0, 1),
+                 "down": (1, 0), "left": (0, -1)}
     dir = directions[0]
-    while right_bound > pos[x] >= 0 and down_bound >= pos[y] >= 0:
+    while right_bound > pos['x'] >= 0 and down_bound >= pos['y'] >= 0:
         step = dir_rules[dir]
-        next_pos = pos + step  # TODO разработать сложение tuples
+        next_pos = (r + q for (r, q) in zip(pos.values(), step))
+        next_pos = dict(zip(('y', 'x'), next_pos))
+        if room[np['y']][np['x']] == 1:
+            ...
+        #TODO make generator from itertools.cycle(directions)
 
 
 def main(file_name: str) -> int:
