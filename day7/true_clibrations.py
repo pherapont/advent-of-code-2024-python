20ad8d1ac738545c3d2ebc2043c4909580085875
pherapont <pherapont@yandex.ru>
2025-10-02 19:58:40 +0300

    begin day 7

---
+++ day7/true_clibrations.py
@@ -0,0 +1,6 @@
+def check_calibration(nums: list[int], res: int) -> bool:
+    expressions = [nums[0]]
+    operators = ("+", "*")
+    for n in nums[1:]:
+        ...
+    # TODO разработать перестановки чисел и операторов. Затем eval()
---
+++ day7/true_clibrations_test.py
@@ -0,0 +1,14 @@
+import unittest
+
+from true_clibrations import check_calibration
+
+
+class TestCalibratons(unittest.TestCase):
+    def test_check_trues_clibration(self):
+        nums = (2, 5, 11)
+        res = 21
+        self.assertTrue(check_calibration(nums, res))
+
+
+if __name__ == "__main__":
+    unittest.main()
