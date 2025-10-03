d99cbf5c777a25d031732d452cbbf78172b7f616
pherapont <pherapont@yandex.ru>
2025-10-03 23:41:00 +0700

    stopor

--- day7/true_clibrations.py
+++ day7/true_clibrations.py
@@ -1,19 +1,21 @@
 from copy import deepcopy
+from pprint import pprint
 
 # TODO test for parse_data()
 
 
-def parse_data(file_name:str) -> list[tuple[int, list[int]]:
+def parse_data(file_name:str) -> list[tuple[int, list[int]]]:
     parsed = [] 
     with open(file_name) as f:
         for line in f:
             result, data = line.split(":")
             data_to_arr = data.split()
-            parsed.append(result.strip(), data_to_arr)
+            parsed.append((int(result.strip()), data_to_arr))
     return parsed
 
 
 def check_calibration(nums: list[int], res: int) -> bool:
+    if len(nums) == 0: return False
     expressions = [ [str(nums[0])], ]
     operators = ("+", "*")
     for n in nums[1:]:
@@ -23,9 +25,24 @@ def check_calibration(nums: list[int], res: int) -> bool:
             for op in operators:
                 unit = [*item, op, str(n)]
                 expressions.append(unit)
+    check = False
     for exp in expressions:
         token = "".join(exp)
         tr = eval(token)
         if tr == res:
-            return True
-    return False
+            check = True
+    return check
+
+
+def main(file_name: str) -> int:
+    total = 0
+    data = parse_data(file_name)
+    for res, token in data:
+        if check_calibration(token, int(res)):
+            total += res
+    return total
+
+
+if __name__ == "__main__":
+    res = parse_data("data_main_test.py")
+    pprint(res)
--- day7/true_clibrations_test.py
+++ day7/true_clibrations_test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from true_clibrations import check_calibration
+from true_clibrations import check_calibration, main
 
 
 class TestCalibratons(unittest.TestCase):
@@ -13,6 +13,9 @@ class TestCalibratons(unittest.TestCase):
         nums = (2, 5, 11)
         res = 20
         self.assertFalse(check_calibration(nums, res))
+        
+    def test_data_from_site(self):
+        self.assertEqual(main("data_main_test.py"), 3749)
 
 if __name__ == "__main__":
     unittest.main()
