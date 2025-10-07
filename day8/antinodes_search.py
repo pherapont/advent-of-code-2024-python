192dee6ada87c4f181fab4db820859efaca6c713
pherapont <pherapont@yandex.ru>
2025-10-07 11:01:53 +0300

    search antinodes

--- day8/antinodes_search.py
+++ day8/antinodes_search.py
@@ -1,4 +1,4 @@
-def search_anitnodes(map: list[list[str]]) -> int:
+def check_nodes(map: list[list[str]]) -> int:
     res: set[list[int]] = {}
     node: str = ""
     out_search: set[str] = {}
@@ -11,3 +11,17 @@ def search_anitnodes(map: list[list[str]]) -> int:
                 current_nodes.append((y, x))
             elif x == node:
                 current_nodes.append((y, x))
+
+
+def search_antinodes(nodes: list[tuple[int]], bounds: tuple[int]) -> list[tuple[int]]:
+    while len(nodes) > 1:
+        chief = nodes.pop(0)
+        for node in nodes:
+            vector = zip(chief, node)
+            diff_positive = [x[1] - x[0] for x in zip(chief, node)]
+            diff_negative = [x[0] - x[1] for x in zip(chief, node)]
+            print(f"chief {chief}")
+            print(f"node {node}")
+            print(f"vector {list(vector)}")
+            print(f"d-n {diff_negative}")
+            print(f"d-p {diff_positive}")
--- day8/antinodes_search_test.py
+++ day8/antinodes_search_test.py
@@ -1,16 +1,25 @@
 import unittest
-from antinodes_search import search_anitnodes
+
+from antinodes_search import check_nodes, search_antinodes
+
 
 class AntinodesTest(unittest.TestCase):
+    def test_check_simple_nodes(self):
+        map = [
+            [".", ".", ".", "."],
+            [".", "0", ".", "."],
+            [".", ".", "0", "."],
+            [".", ".", ".", "."],
+        ]
+        res = [[0, 0], [3, 3]]
+        self.assertEqual(check_nodes(map), res)
 
     def test_search_simple_antinodes(self):
-        map = [[".", ".", ".", "."],
-               [".", "0", ".", "."],
-               [".", ".", "0", "."],
-               [".", ".", ".", "."]]
+        nodes = [(1, 1), (2, 2)]
+        field_size = (4, 4)
         res = [[0, 0], [3, 3]]
-        self.assertEqual(search_anitnodes(map), res)
+        self.assertEqual(search_antinodes(nodes, field_size), res)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
