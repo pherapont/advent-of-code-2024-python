ebf9736eae9f3b12196b8a044154dd9865b078c6
pherapont <pherapont@yandex.ru>
2025-10-10 14:45:55 +0300

    create v1 disk_defragmenter

--- day9/disk_fragmenter.py
+++ day9/disk_fragmenter.py
@@ -1,4 +1,4 @@
-def get_data_from_file(file_name:str) -> tuple[int]:
+def get_data_from_file(file_name: str) -> tuple[int]:
     data = []
     data_str: str
     with open(file_name) as f:
@@ -9,12 +9,33 @@ def get_data_from_file(file_name:str) -> tuple[int]:
 
 
 def get_disk_map(disk_desc: tuple[int]) -> tuple[int]:
-    map: list[int] = []
+    disk_map: list[int] = []
     gap_id = -1
     for i, num in enumerate(disk_desc):
         for j in range(num):
             if i % 2:
-                map.append(gap_id)
+                disk_map.append(gap_id)
             else:
-                map.append(i / 2)
-    return tuple(map)
+                disk_map.append(i / 2)
+    return tuple(disk_map)
+
+
+def defragment_disk(disk_map_data: tuple[int]) -> tuple[int]:
+    disk_map = list(disk_map_data)
+    direct_counter = 0
+    back_counter = len(disk_map) - 1
+    while direct_counter != back_counter:
+        if disk_map[direct_counter] == -1:
+            if disk_map[back_counter] == -1:
+                back_counter -= 1
+                continue
+            else:
+                disk_map[direct_counter] = disk_map[back_counter]
+                back_counter -= 1
+                direct_counter += 1
+        else:
+            direct_counter += 1
+            continue
+    return tuple(disk_map[:direct_counter])
+
+    return ()
--- day9/disk_fragmenter_test.py
+++ day9/disk_fragmenter_test.py
@@ -1,15 +1,22 @@
 import unittest
-from disk_fragmenter import get_disk_map, get_data_from_file
+
+from disk_fragmenter import defragment_disk, get_data_from_file, get_disk_map
+
 
 class TestDiskFragmenter(unittest.TestCase):
     def test_get_simple_disk_map(self):
         disk_desc = (1, 2, 3, 4, 5)
-        map = (0, -1, -1, 1, 1, 1, -1, -1, -1, -1, 2, 2, 2, 2, 2)
-        self.assertEqual(get_disk_map(disk_desc), map)
+        disk_map = (0, -1, -1, 1, 1, 1, -1, -1, -1, -1, 2, 2, 2, 2, 2)
+        self.assertEqual(get_disk_map(disk_desc), disk_map)
 
     def test_get_data_from_simple_file(self):
-        self.assertEqual(get_data_from_file("data_simple_test.txt"),
-                         (1, 2, 3, 4, 5))
+        self.assertEqual(get_data_from_file("data_simple_test.txt"), (1, 2, 3, 4, 5))
+
+    def test_defragment_simple_disk(self):
+        disk_map = (0, -1, -1, 1, 1, 1, -1, -1, -1, -1, 2, 2, 2, 2, 2)
+        res = (0, 2, 2, 1, 1, 1, 2, 2, 2)
+        self.assertEqual(defragment_disk(disk_map), res)
+
 
 if __name__ == "__main__":
     unittest.main()
