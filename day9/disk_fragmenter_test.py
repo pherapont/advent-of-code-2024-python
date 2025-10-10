fb265727a98abfb6ad0a07b065683a24c6a6d204
pherapont <pherapont@yandex.ru>
2025-10-10 22:24:54 +0700

    нейдена потеря элемента при defragment_disk

--- day9/disk_fragmenter_test.py
+++ day9/disk_fragmenter_test.py
@@ -10,11 +10,13 @@ from disk_fragmenter import (
 
 
 class TestDiskFragmenter(unittest.TestCase):
+    @unittest.skip("simple input")
     def test_get_simple_disk_map(self):
         disk_desc = (1, 2, 3, 4, 5)
         disk_map = (0, -1, -1, 1, 1, 1, -1, -1, -1, -1, 2, 2, 2, 2, 2)
         self.assertEqual(get_disk_map(disk_desc), disk_map)
 
+    @unittest.skip("simple input")
     def test_get_data_from_simple_file(self):
         self.assertEqual(get_data_from_file("data_simple_test.txt"), (1, 2, 3, 4, 5))
 
@@ -23,14 +25,35 @@ class TestDiskFragmenter(unittest.TestCase):
         res = (0, 2, 2, 1, 1, 1, 2, 2, 2)
         self.assertEqual(defragment_disk(disk_map), res)
 
+    @unittest.skip("simple input")
     def test_get_simple_checksum(self):
         disk = (0, 2, 2, 1, 1, 1, 2, 2, 2)
         res = 60
         self.assertEqual(get_check_sum(disk), res)
 
+    @unittest.skip("simple input")
     def test_complex_simple_input(self):
         self.assertEqual(main("data_simple_test.txt"), 60)
 
+    def test_get_second_disk_map(self):
+        disk_desc = (2, 3, 3, 3, 1, 3, 3, 1, 2, 1, 4, 1, 4, 1, 3, 1, 4, 0, 2)
+        disk_map = (0, 0, -1, -1, -1, 1, 1, 1, -1, -1, -1, 2, -1, -1, -1, 3, 3, 3, -1,
+                    4, 4, -1, 5, 5, 5, 5, -1, 6, 6, 6, 6, -1, 7, 7, 7, -1, 8, 8, 8, 
+                    8, 9, 9)
+        self.assertEqual(get_disk_map(disk_desc), disk_map)
+
+    #TODO: найти потерю последнего элемента при дефрагментации и выяснить почему небыло потери в test_defragment_simple_disk()
+    
+    def test_defragment_second_disk(self):
+        disk_map = (0, 0, -1, -1, -1, 1, 1, 1, -1, -1, -1, 2, -1, -1, -1, 3, 3, 3, -1,
+                    4, 4, -1, 5, 5, 5, 5, -1, 6, 6, 6, 6, -1, 7, 7, 7, -1, 8, 8, 8, 
+                    8, 9, 9)
+        res = (0, 0, 9, 9, 8, 1, 1, 1, 8, 8, 8, 2, 7, 7, 7, 3, 3, 3, 6, 4, 4, 6,
+               5, 5, 5, 5, 6, 6)
+        self.assertEqual(defragment_disk(disk_map), res)
+
+
+    @unittest.skip("complex_test")
     def test_complex_second_input(self):
         self.assertEqual(main("data_second_test.txt"), 1928)
 
