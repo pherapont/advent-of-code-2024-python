0ad97a92c1e4a4cc635d7fff97e00231007071ac
pherapont <pherapont@yandex.ru>
2025-10-21 09:47:23 +0300

    implement func for get data from txt

--- day10/hiking_trails.py
+++ day10/hiking_trails.py
@@ -1,23 +1,33 @@
+def get_trail_map(file_name: str) -> list[list[int]]:
+    t_map = []
+    with open(file_name) as f:
+        for line in f:
+            t_map.append([int(i) for i in list(line.strip())])
+    return t_map
+
+
 def get_trail_score(t_map: list[list[int]], t_head: tuple[int]) -> int:
     """
         Задача: найти в окружении текущих точек точки со значением на 1 больше.
         Записать все пути от t_head до новой точки.
+        !! Ошибка: надо искать не количество уникальных путей, а количество достижимых точек 9.
+        Поэтому в спсок включаем только уникальные точки.
         Вообще путь полностью записывать не нужно, можно только корректную последнюю точку.
         Если у точки нет соседей +1, то удалить путь до этой точки из списка.
     """
     steps = [(0, 1), (0, -1), (1, 0), (-1, 0)]
     h_bound = len(t_map[0])
     v_bound = len(t_map)
-    trails = [t_head]
+    trails = {t_head}
     for point in range(1, 10):
         tmp_trails = trails.copy()
-        trails = []
+        trails = set()
         for last in tmp_trails:
             for step in steps:
                 next = list(sum(x) for x in zip(last, step))
                 y, x = next
                 if y in range (v_bound) and x in range(h_bound) and t_map[y][x] == point:
-                    trails.append((y, x))
+                    trails.add((y, x))
         print(f"{point=}--{trails=}")
     return len(trails)
 
--- day10/hiking_trails_test.py
+++ day10/hiking_trails_test.py
@@ -1,4 +1,4 @@
-from hiking_trails import get_trail_score
+from hiking_trails import get_trail_score, get_trail_map
 from data_simple_test import t_map as simple_t_map
 from data_middle_test import t_map as middle_t_map
 
@@ -7,3 +7,6 @@ def test_get_simple_trail_score():
 
 def test_get_middle_trail_score():
     assert get_trail_score(middle_t_map, (0, 2)) == 5
+
+def test_get_data():
+    assert get_trail_map("data_middle_test.txt") == middle_t_map
