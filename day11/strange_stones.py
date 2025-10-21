d3b4c3089580b6fd98ec5110ec86717e06fe0e73
pherapont <pherapont@yandex.ru>
2025-10-21 19:46:37 +0300

    day11 begin

---
+++ day11/strange_stones.py
@@ -0,0 +1,12 @@
+def transform_stones(stones: list[str], blinks: int) -> list[str]:
+    """
+    1) If the stone is engraved with the number 0, it is replaced by a stone engraved with the number 1.
+    2) If the stone is engraved with a number that has an even number of digits,
+        it is replaced by two stones.
+        The left half of the digits are engraved on the new left stone,
+        and the right half of the digits are engraved on the new right stone.
+        (The new numbers don't keep extra leading zeroes: 1000 would become stones 10 and 0.)
+    3) If none of the other rules apply, the stone is replaced by a new stone; 
+        the old stone's number multiplied by 2024 is engraved on the new stone.
+    """
+    ...
---
+++ day11/strange_stones_test.py
@@ -0,0 +1,7 @@
+import pytest
+from strange_stones import transform_stones
+
+def test_transform_simple_stones():
+    init_stones = ["0", "1", "10", "99", "999"]
+    transformed = ["1", "2024", "1", "0", "9", "9", "2021976"]
+    assert transform_stones(init_stones, 1) == transformed
