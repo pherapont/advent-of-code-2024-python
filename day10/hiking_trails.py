4dabd7960da0c190daffe327acd1971e835ac901
pherapont <pherapont@yandex.ru>
2025-10-21 10:31:06 +0300

    finish day 10

--- day10/hiking_trails.py
+++ day10/hiking_trails.py
@@ -30,14 +30,39 @@ def get_trail_score(t_map: list[list[int]], t_head: tuple[int]) -> int:
                     trails.add((y, x))
     return len(trails)
 
+def get_trail_raiting(t_map: list[list[int]], t_head: tuple[int]) -> int:
+    """
+        Задача: найти в окружении текущих точек точки со значением на 1 больше.
+        Записать все пути от t_head до новой точки.
+        в этом задании верно
+        Вообще путь полностью записывать не нужно, можно только корректную последнюю точку.
+        Если у точки нет соседей +1, то удалить путь до этой точки из списка.
+    """
+    steps = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+    h_bound = len(t_map[0])
+    v_bound = len(t_map)
+    trails = [t_head]
+    for point in range(1, 10):
+        tmp_trails = trails.copy()
+        trails = []
+        for last in tmp_trails:
+            for step in steps:
+                next = list(sum(x) for x in zip(last, step))
+                y, x = next
+                if y in range (v_bound) and x in range(h_bound) and t_map[y][x] == point:
+                    trails.append((y, x))
+    return len(trails)
+
 def main(file_name: str) -> int:
     t_map = get_trail_map(file_name)
     res = 0
     for y, line in enumerate(t_map):
         for x, elem in enumerate(line):
             if elem == 0:
-                score = get_trail_score(t_map, (y, x))
-                res += score
+                #score = get_trail_score(t_map, (y, x))
+                raiting = get_trail_raiting(t_map, (y, x))
+                #res += score
+                res += raiting
     return res
 
 if __name__ == "__main__":
--- day10/hiking_trails_test.py
+++ day10/hiking_trails_test.py
@@ -1,4 +1,5 @@
-from hiking_trails import get_trail_score, get_trail_map, main
+import pytest
+from hiking_trails import get_trail_score, get_trail_map, get_trail_raiting, main
 from data_simple_test import t_map as simple_t_map
 from data_middle_test import t_map as middle_t_map
 
@@ -11,5 +12,12 @@ def test_get_middle_trail_score():
 def test_get_data():
     assert get_trail_map("data_middle_test.txt") == middle_t_map
 
-def test_main():
+@pytest.mark.skip(reason="this test for part 1")
+def test_main_score():
     assert main("data_middle_test.txt") == 36
+
+def test_get_middle_trail_raiting():
+    assert get_trail_raiting(middle_t_map, (0, 2)) == 20
+
+def test_main_rainting():
+    assert main("data_middle_test.txt") == 81
