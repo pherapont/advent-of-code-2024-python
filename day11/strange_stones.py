177f46ffd50f1b03816614e385c79c52f15e665d
pherapont <pherapont@yandex.ru>
2025-10-21 21:32:45 +0300

    implement transform simple stone

--- day11/strange_stones.py
+++ day11/strange_stones.py
@@ -1,4 +1,4 @@
-def transform_stones(stones: list[str], blinks: int) -> list[str]:
+def transform_stones(stones: tuple[str], blinks: int) -> list[str]:
     """
     1) If the stone is engraved with the number 0, it is replaced by a stone engraved with the number 1.
     2) If the stone is engraved with a number that has an even number of digits,
@@ -9,4 +9,20 @@ def transform_stones(stones: list[str], blinks: int) -> list[str]:
     3) If none of the other rules apply, the stone is replaced by a new stone; 
         the old stone's number multiplied by 2024 is engraved on the new stone.
     """
-    ...
+    new_stones = list(stones)
+    for bl in range(blinks):
+        prev_stones = new_stones.copy()
+        new_stones = []
+        for stone in prev_stones:
+            if stone == "0":
+                new_stones.append("1")
+            elif len(stone) and not len(stone) % 2:
+                mid = len(stone) // 2
+                first = stone[:mid]
+                new_stones.append(first)
+                last = stone[mid:]
+                new_stones.append(last)
+            else:
+                new_stone = str(int(stone) * 2024)
+                new_stones.append(new_stone)
+    return new_stones
--- day11/strange_stones_test.py
+++ day11/strange_stones_test.py
@@ -2,6 +2,6 @@ import pytest
 from strange_stones import transform_stones
 
 def test_transform_simple_stones():
-    init_stones = ["0", "1", "10", "99", "999"]
+    init_stones = ("0", "1", "10", "99", "999")
     transformed = ["1", "2024", "1", "0", "9", "9", "2021976"]
     assert transform_stones(init_stones, 1) == transformed
